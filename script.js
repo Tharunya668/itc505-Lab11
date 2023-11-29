2eac077a677dea5875c198dfe32c95df4b81cf34
Tharunya668 <144745589+Tharunya668@users.noreply.github.com>
2023-11-30 03:04:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.post('/cs212/lab/7', (req, res) => {
+    const noun = req.body.noun;
+    const adjective = req.body.adjective;
+    const verb = req.body.verb;
+    const pluralNoun = req.body.pluralNoun;
+    const color = req.body.color;
+    const adverb = req.body.adverb;
+    const bodyPart = req.body.bodyPart;
+    const emotion = req.body.emotion;
+    const animal = req.body.animal;
+    const food = req.body.food;
+
+    const madLib = `The ${adjective} ${noun} ${verb} over the ${color} ${pluralNoun}. They were ${adverb} ${emotion} when they saw a ${animal} with a ${bodyPart} eating ${food}.`;
+
+    // Send the madLib response back to the client
+    res.send(`<h1>Mad Lib Result</h1><p>${madLib}</p>`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
